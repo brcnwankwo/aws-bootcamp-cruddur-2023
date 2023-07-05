41087e5eeae7b9fbc715871ad5c19d0413827384
brcnwankwo <brcnwankwo@gmail.com>
2023-07-05 23:08:18 +0000

    cdk bucket policy

--- thumbing-serverless-cdk/lib/thumbing-serverless-cdk-stack.ts
+++ thumbing-serverless-cdk/lib/thumbing-serverless-cdk-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
 import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as s3n from 'aws-cdk-lib/aws-s3-notifications';
+import * as iam from 'aws-cdk-lib/aws-iam';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import { Construct } from 'constructs';
 import * as dotenv from 'dotenv';
@@ -31,6 +32,8 @@ export class ThumbingServerlessCdkStack extends cdk.Stack {
 
     this.createS3NotifyToLambda(folderInput,lambda,bucket)
 
+    const s3ReadWritePolicy = this.createPolicyBucketAccess(bucket.bucketArn)
+    lambda.addToRolePolicy(s3ReadWritePolicy);
   }
 
   createBucket(bucketName: string): s3.IBucket {
@@ -67,5 +70,17 @@ export class ThumbingServerlessCdkStack extends cdk.Stack {
       destination,
       {prefix: prefix} // folder to contain the original images
     )
+  }
+  createPolicyBucketAccess(bucketArn: string){
+    const s3ReadWritePolicy = new iam.PolicyStatement({
+      actions: [
+        's3:GetObject',
+        's3:PutObject',
+      ],
+      resources: [
+        `${bucketArn}/*`,
+      ]
+    });
+    return s3ReadWritePolicy;
   }
 }
